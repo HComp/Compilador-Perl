fa2afb5bd625c023fff5e97d7221020cca5e08d3
Diegores14 <diegorestrepo68@utp.edu.co>
2017-09-25 22:38:55 -0500

    analizador lexico al 90% totalmente funcional

--- AnalisadorLexico.py
+++ AnalisadorLexico.py
@@ -17,12 +17,20 @@ tokens = [
     'GREATER',
     'GREATERGREATER',
     'GREATEREQUAL',
+    'GUIONDOWN',
+    'ARROW',
+    'EQUALGREATER',
+    'LESSEQUALGREATER',
+    'EQUALPRIME',
+    'EXCLAMATIONPRIME',
     'EQUAL',
     'DEQUAL',
     'DISTINT',
     'ISEQUAL',
     'SEMICOLON',
     'COMMA',
+    'PLUSGREATER',
+   	'PLUSLESS',
     'LPAREN',
     'RPAREN',
     'LBRACKET',
@@ -30,23 +38,35 @@ tokens = [
     'LBLOCK',
     'RBLOCK',
     'COLON',
-    'AMPERSANT',
-    'HASHTAG',
+    'COMMENT',
     'DOT',
-    # Others   
-    'ESCALARES',
-    'ARRAY',
-    'ASOCIADAS', 
+    # logic bitwise operators
+    'ANDBW',
+    'ORBW',
+    'XOR',
+    'COMPLEMENT',
+    # logic operators
+    'ORS',
+    'ANDS',
+    'NOTS',
+    'OR',
+    'AND',
+    'NOT',
+    # Others  
+    'INTERROGATION', 
+    'ID',
+    'DOLLARDOLLAR',
     'ENTERO',
     'HEXADECIMAL',
     'OCTAL',
     'CADENA',
+    'CADENAA',
     'REFERENCIAS',
     'REFERENCIALISTA',
-    'PREINCREMENTO',
-    'POSINCREMENTO',
-    'PREDECREMENTO',
-    'POSDECREMENTO',
+    'INCREMENTO',
+    'DECREMENTO',
+    'PREINDEX',
+    'POSTINDEX',
 
     #bucles and conditional
     'FOR',
@@ -57,7 +77,8 @@ tokens = [
 	'NEXT',					#NEXT sirve para continuaren un bucle 
 	'IF',
 	'ELSE',
-
+	'ELSIF',
+	'GOTO',
 
 	#encontrado en la pagina https://www.programacionfacil.com/perl_script/palabras_reservadas.html
 	'ABS', 					#devuelve el valor absoluto de la expresion pasada.
@@ -73,6 +94,7 @@ tokens = [
 	'EXEC', 				#ejecuta lo que pasemos como parametro y sale del programa.
 	'EXIT', 				#hace que salgamos del perl script devolviendo al sistema operativo el valor pasado como argumento.
 	'EXP', 					#retorna el numero e elevado a la potencia pasada como parametro.
+	'EACH',
 	'FILENO', 			#devuelve el descriptor del manejador del fichero pasado como parametro.
 	'FORK', 				#realiza una llamada fork. getc: lee el siguiente caracter del fichero especificado.
 	'HEX', 					#devuelve el valor decimal del numero hexadecimal pasado como parametro.
@@ -94,19 +116,31 @@ tokens = [
 	'RENAME', 			#sirve para renombrar un fichero.
 	'REQUIRE', 			#sirve para incluir codigo externo en nuestro guion.
 	'RETURN', 			#devuelve un valor desde una subrutina.
+	'REFERENCE',
 	'RMDIR', 				#borra un directorio.
 	'SEEK', 				#situa un puntero a fichero en un lugar determinado.
-	'SELECT', 			#sirve para seleccionar el manejador de fichero que sera utilizado por defecto para la salida de los comandos o funciones que no especifiquen un determinado manejador de fichero como parametro.
+	'SELECT', 				#sirve para seleccionar el manejador de fichero que sera utilizado por defecto para la salida de los comandos o funciones que no especifiquen un determinado manejador de fichero como parametro.
 	'SHIFT', 				#devuelve el primer valor del array dado borrandolo posteriormente.
 	'SIN', 					#devuelve el seno del angulo pasado en radianes.
 	'SLEEP', 				#causa que el perl script o guion se detenga el numero de segundos especificados.
-	'sort', 				#ordena el array dado.
+	'SORT', 				#ordena el array dado.
 	'SPLIT', 				#divide una cadena en subcadenas segun el separador especificado.
 	'SQRT', 				#devuelve la raiz cuadrada del numero pasado.
 	'SYSTEM', 			#igual que exec pero no se sale del perl script.
+	'SUB',
 	'TELL', 				#devuelve la posicion actual del puntero a fichero del manejador de fichero especificado.
 	'VALUES', 			#devuelve todos los valores del array asociativo dado.
 	'WRITE', 				#escribe un registro con formato en el fichero asociado a ese formato.
+	'CMP',
+	'RINDEX',
+	'SUBSTR',
+	'PACK',
+	'UNSHIFT',
+	'UNTIL',
+	'UNLESS',
+	'USE',
+	'REVERSE',
+
 	'PESOS',				#para escribir un escalar o una referencia
 	'ARROBA',				#para definir un arreglo
 	'PORCENTAJE'			#para definir un asociado
@@ -115,7 +149,9 @@ tokens = [
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
+t_GUIONDOWN = r'_'
 t_DIVIDE = r'/'
+t_REFERENCE =r'\\'
 t_LESS = r'<'
 t_GREATER = r'>'
 t_EQUAL = r'='
@@ -129,12 +165,32 @@ t_RBRACKET = r'\]'
 t_LBLOCK = r'\{'
 t_RBLOCK = r'\}'
 t_COLON = r':'
-t_AMPERSANT = r'\&'
-t_HASHTAG = r'\#'
 t_DOT = r'\.'
 t_PESOS = r'\$'
 t_ARROBA = r'\@'
 t_PORCENTAJE = r'\%'
+t_NOT = r'\!'
+t_INTERROGATION = r'\?'
+t_COMPLEMENT = r'\~'
+t_ANDBW = r'\&'
+t_ORBW = r'\|'
+t_XOR = r'\^'
+
+
+def t_ORS(t):
+	r'or'
+	return t
+
+def t_ANDS(t):
+	r'and'
+	return t
+	
+def t_NOTS(t):
+	r'not'
+	return t
+
+def t_COMMENT(t):
+	r'\#.*'
 
 def t_FOR(t):
 	r'for'
@@ -168,7 +224,9 @@ def t_ELSE(t):
 	r'else'
 	return t
 
-
+def t_ELSIF(t):
+	r'elsif'
+	return t
 
 def t_ABS(t):
 	r'abs'
@@ -221,6 +279,10 @@ def t_EXP(t): 					#retorna el numero e elevado a la potencia pasada como parame
 	r'exp'
 	return t
 
+def t_EACH(t):
+	r'each'
+	return t
+
 def t_FILENO(t): 			#devuelve el descriptor del manejador del fichero pasado como parametro.
 	r'fileno'
 	return t
@@ -229,12 +291,28 @@ def t_FORK(t): 				#realiza una llamada fork. getc: lee el siguiente caracter de
 	r'fork'
 	return t
 
+def t_GOTO(t):
+	r'goto'
+	return t
+
 def t_HEX(t): 					#devuelve el valor decimal del numero hexadecimal pasado como parametro.
 	r'hex'
 	return t
 
 def t_INDEX(t): 				#devuelve la posicion de la primera ocurrencia de una cadena en otra.
 	r'index'
+	return t
+
+def t_RINDEX(t):
+	r'rindex'
+	return t
+
+def t_SUBSTR(t):
+	r'substr'
+	return t
+
+def t_SUB(t):
+	r'sub'
 	return t
 
 def t_INT(t): 					#devuelve la parte entera del parametro pasado.
@@ -270,7 +348,7 @@ def t_OCT(t): 					#devuelve el valor decimal del numero octal pasado como param
 	return t
 
 def t_OPEN(t): 				#abre el fichero fichero dado asociandole un manejador de fichero especificado tambien como parametro.
-	r'openr'
+	r'open'
 	return t
 
 def t_POP(t): 					#retorna y borra el ultimo elemento del array dado.
@@ -283,6 +361,26 @@ def t_PRINT(t): 				#muestra en la salida standard o en el fichero especificado 
 
 def t_PUSH(t): 				#anade el valor dado al final del array pasado como parametro.
 	r'push'
+	return t
+
+def t_PACK(t):
+	r'pack'
+	return t
+
+def t_PREINDEX(t):
+	r'\$\['
+	return t
+
+def t_POSTINDEX(t):
+	r'\$\#'
+	return t
+
+def t_PLUSGREATER(t):
+	r'\+\>'
+	return t
+
+def t_PLUSLESS(t):
+	r'\+\<'
 	return t
 
 def t_RAND(t): 				#devuelve un numero aleatorio entre 0 y el valor pasado como argumento.
@@ -305,12 +403,16 @@ def t_RETURN(t): 			#devuelve un valor desde una subrutina.
 	r'return'
 	return t
 
+def t_REVERSE(t):
+	r'reverse'
+	return t
+
 def t_RMDIR(t): 				#borra un directorio.
 	r'rmdir'
 	return t
 
-def t_SEE(t): 				#situa un puntero a fichero en un lugar determinado.
-	r'see'
+def t_SEEK(t): 				#situa un puntero a fichero en un lugar determinado.
+	r'seek'
 	return t
 
 def t_SELECT(t): 			#sirve para seleccionar el manejador de fichero que sera utilizado por defecto para la salida de los comandos o funciones que no especifiquen un determinado manejador de fichero como parametro.
@@ -329,7 +431,7 @@ def t_SLEEP(t):				#causa que el perl script o guion se detenga el numero de seg
 	r'slepp'
 	return t
 
-def t_sort(t): 				#ordena el array dado.
+def t_SORT(t): 				#ordena el array dado.
 	r'sort'
 	return t
 
@@ -349,6 +451,22 @@ def t_TELL(t): 				#devuelve la posicion actual del puntero a fichero del maneja
 	r'tell'
 	return t
 
+def t_UNSHIFT(t):
+	r'unshift'
+	return t
+
+def t_UNTIL(t):
+	r'until'
+	return t
+
+def t_UNLESS(t):
+	r'unless'
+	return t
+
+def t_USE(t):
+	r'use'
+	return t
+
 def t_VALUES(t): 			#devuelve todos los valores del array asociativo dado.
 	r'values'
 	return t
@@ -357,27 +475,30 @@ def t_WRITE(t):				#escribe un registro con formato en el fichero asociado a ese
 	r'write'
 	return t
 
-def t_ARRAY(t): 			#haciendo caso a la recomendacion de reconocer el @ como un token aparte
-	r'[a-zA-Z][a-zA-Z_0-9]+' #se le puede anteponer el simbolo $. opcional
+def t_ID(t):
+	r'[a-zA-Z][a-zA-Z_0-9]*'
 	return t
 
-def t_ASOCIADAS(t): 		#haciendo caso a la recomendacion de reconocer el % como un token aparte
-	r'[a-zA-Z][a-zA-Z_0-9]+' #hace falta algo
+def t_DOLLARDOLLAR(t):
+	r'\$\$'
 	return t
 
 def t_ENTERO(t):
-	r'[1-9]\d+'
+	r'([1-9][0-9]*|0)'
 	return t
-
 # Aqui tambien puede ser 0X o 0x
 def t_HEXADECIMAL(t):
-	r'0x[a-fA-F0-9]+'
+	r'(0x|0X)[a-fA-F0-9]+'
 	return t
 
 # aqui falta poner cadenas pero con apostrofes ''
 def t_CADENA(t):
-	r'\".*\"'
-	return t	
+	r'\"[a-zA-Z_0-9\&\.\-\_\+\*\$\%\@\!\xc2\xa1\/\\\#\?\xc2\xbf\(\)\|\=\{\}\[\]\>\<\,\: \t]*\"' 
+	return t
+
+def t_CADENAA(t):
+	r'\'[a-zA-Z_0-9\&\.\-\_\+\*\$\%\@\!\xc2\xa1\/\\\#\?\xc2\xbf\(\)\|\=\{\}\[\]\>\<\,\: \t]*\'' 
+	return t
 
 def t_OCTAL(t):
 	r'0\d+'
@@ -395,12 +516,39 @@ def t_LESSEQUAL(t):
 	r'<='
 	return t
 
-def GREATERGREATER(t):
+def t_GREATERGREATER(t):
 	r'>>'
+	return t
+
+def t_EQUALGREATER(t):
+	r'=>'
 	return t
 
 def t_GREATEREQUAL(t):
 	r'>='
+	return t
+
+def t_LESSEQUALGREATER(t):
+	r'<=>'
+	return t
+
+def t_EQUALPRIME(t):
+	r'=~'
+	return t
+
+def t_EXCLAMATIONPRIME(t):
+	r'!~'
+	return t
+
+def t_AND(t):
+	r'\&\&'
+	return t
+def t_OR(t):
+	r'\|\|'
+	return t
+
+def t_ARROW(t):
+	r'->'
 	return t
 
 def t_DEQUAL(t):
@@ -415,43 +563,17 @@ def t_PLUSPLUS(t):
 	r'\+\+'
 	return t
 
+def t_INCREMENTO(t):
+	r'\+\+'
+	return t
+
+def t_DECREMENTO(t):
+	r'\-\-'
+	return t
+
 def t_newline(t):
 	r'\n'
 	t.lexer.lineno += 1
-
-
-def t_ESCALARES(t): #esta parte reconoce los escalares con el simbolo $, se va a probar sin recibir el simbolo $
-	r'\$[a-zA-Z][a-zA-Z_0-9]+'
-	return t
-
-def t_REFERENCIAS(t): #aqui tengo duda como se debe utilizar el simbolo @
-	r'\$\$[a-zA-Z][a-zA-Z_0-9]+'
-	return t
-
-def t_REFERENCIALISTA(t):
-	r'\$[a-zA-Z][a-zA-Z_0-9]+\-\>' #forma abreviada de utilizar referencias a listas asociadas o arrays
-	return t 					   #hace falta la referencia a arreglos 2d y 3d
-
-#No se si hacer expresion regular para operadores aritmeticos, ++ --
-#Operadores aritmeticos
-def t_PREINCREMENTO(t):
-	r'\+\+[a-zA-Z]+'
-	return t
-
-def t_POSINCREMENTO(t):
-	r'[a-zA-Z]+\+\+'
-	return t
-
-def t_PREDECREMENTO(t):
-	r'\-\-[a-zA-Z]+'
-	return t
-
-def t_POSDECREMENTO(t):
-	r'[a-zA-Z]+\-\-'
-	return t
-###########################################
-
-#Operadores relacionales
 
 
 
